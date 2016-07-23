a43a48ff950ee98f010987ff394a1e812a313190
Vivek Maharajh <vivmaha@live.com>
2016-07-22 22:27:22 -0700

    added gameboard

---
+++ Trapt/Assets/Board/Board.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Board : MonoBehaviour {
+    public GameObject originTile;
+    public int size = 5;
+	void Start () {
+        
+	    for (int i = -this.size; i <= this.size; i++)
+        {
+            for (int j = -this.size; j <= this.size; j++)
+            {
+                var newTile = Instantiate(originTile);
+                
+                newTile.transform.position = new Vector2(originTile.transform.position.x + i, originTile.transform.position.y + j * (float)(Math.Sqrt(3) / 2.0));
+                
+                if (Math.Abs(j) % 2 == 1)
+                {
+                    newTile.transform.position += new Vector3(0.5f, 0, 0);
+                }
+
+                newTile.transform.parent = this.transform;
+            }
+        }
+        Destroy(originTile);
+	}	
+}
