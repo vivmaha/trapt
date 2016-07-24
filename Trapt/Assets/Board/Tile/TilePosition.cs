62d9bb0fe34b16a3729f806f5ae768f25bcdb444
Vivek Maharajh <vivmaha@live.com>
2016-07-23 20:28:20 -0700

    refactored TilePosition

---
+++ Trapt/Assets/Board/Tile/TilePosition.cs
@@ -0,0 +1,30 @@
+﻿class TilePosition
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+
+    public override int GetHashCode()
+    {
+        return this.X.GetHashCode() ^ this.Y.GetHashCode();
+    }
+
+    public override bool Equals(object obj)
+    {
+        TilePosition other = (TilePosition)obj;
+        if (other == null)
+        {
+            return false;
+        }
+
+
+        return
+            this.X == other.X &&
+            this.Y == other.Y;
+    }
+
+    public TilePosition(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+}
